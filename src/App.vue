f43ecc94313cbf25d5a50d90466898ab05fdfce5
Christopher Graham <chgraham@springfieldclinic.com>
2023-10-30 10:57:15 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+	<div class="flex flex-col justify-center items-center">
+		<img class="w-96 h-96" alt="Planet Of Design logo" src="./assets/PlanetOfDesign_logo.png" />
+		<h1 class="text-3xl font-bold text-pod">
+			Planet Of Design
+		</h1>
+	</div>
+</template>
+
+<style scoped></style>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        pod: '#82d55b'
+      },
+    },
+  },
+  plugins: [],
+}
+
